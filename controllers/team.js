9f1f75f867d4ac2cde3bc1d93e4528cea0679277
Simone Teglia <simone.teglia9@gmail.com>
2023-03-07 15:12:03 +0100

    Change team controller

--- controllers/team.js
+++ controllers/team.js
@@ -1,9 +1,9 @@
 import { db } from "../db.js";
 
 export const getTeamYear = (req, res) => {
-  const q = "SELECT * FROM team22 WHERE anno = ?";
-  db.query(q, req.params.anno, (err, data) => {
-    if (err) return res.send(err);
-    return res.status(200).json(data);
-  });
+	const q = `SELECT * FROM team${req.params.anno}`;
+	db.query(q, req.params.anno, (err, data) => {
+		if (err) return res.send(err);
+		return res.status(200).json(data);
+	});
 };
