eee49befe13edfa84bf16d8f969bba6907d1a6fa
Gloria Marinelli <gloria.marinelli22@gmail.com>
2023-03-24 11:20:25 +0100

    .

--- routes/newsletter.js
+++ routes/newsletter.js
@@ -4,6 +4,6 @@ import { postNewsletter, getNewsletter } from "../controllers/newsletter.js";
 const router = express.Router();
 
 router.post("/", postNewsletter);
-router.get("/newsletter", getNewsletter);
+router.get("/", getNewsletter);
 
 export default router;
