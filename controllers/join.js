2e4144589a98e18da067b0efe3b048350d0ed964
Simone Teglia <simone.teglia9@gmail.com>
2023-03-25 11:27:13 +0100

    join us parter + join us advise

--- controllers/join.js
+++ controllers/join.js
@@ -16,12 +16,31 @@ export const joinStudent = (req, res) => {
 	});
 };
 
-// export const join1 = (req, res) => {
-// 	const q =
-// 		"INSERT INTO join1 (`email`,`nome`,`telefono`,`messaggio`) VALUES (?)";
+export const joinPartner = (req, res) => {
+	const q =
+		"INSERT INTO joinuspartner (`email`,`nome`,`telefono`,`contenuto`) VALUES (?)";
 
-// 	db.query(q, (err, data) => {
-// 		if (err) return res.status(500).json(err);
-// 		return res.status(200).json("Controlla i campi!");
-// 	});
-// };
+	const values = [
+		req.body.email,
+		req.body.nome,
+		req.body.telefono,
+		req.body.contenuto,
+	];
+
+	db.query(q, [values], (err, data) => {
+		if (err) return res.status(500).json(err);
+		return res.status(200).json("OK");
+	});
+};
+
+export const joinAdvise = (req, res) => {
+	const q =
+		"INSERT INTO joinusadvise (`nome`,`telefono`,`contenuto`) VALUES (?)";
+
+	const values = [req.body.nome, req.body.telefono, req.body.contenuto];
+
+	db.query(q, [values], (err, data) => {
+		if (err) return res.status(500).json(err);
+		return res.status(200).json("OK");
+	});
+};
