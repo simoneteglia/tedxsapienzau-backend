0300f0ec2f432d9b7c62a66d7ea38884adffa15a
Simone Teglia <simone.teglia9@gmail.com>
2023-04-03 11:20:00 +0200

    param anno

--- routes/team.js
+++ routes/team.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { getTeam} from "../controllers/team.js";
+import { getTeam } from "../controllers/team.js";
 
 const router = express.Router();
 
-router.get("/",  getTeam);
+router.get("/:anno", getTeam);
 
 export default router;
