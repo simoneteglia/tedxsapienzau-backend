41782fef01dbc1f7ac07122b2ca9b06b523e4682
Gloria Marinelli <gloria.marinelli123@gmail.com>
2024-10-23 13:07:04 +0200

    update

--- db.js
+++ db.js
@@ -3,33 +3,45 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const configuration = {
-	host: process.env.HOST,
-	user: process.env.USERNAME,
-	password: process.env.PASSWORD,
-	port: "3306",
-	database: process.env.DATABASE,
+  host: process.env.HOST,
+  user: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  port: "3306",
+  database: process.env.DATABASE,
+  connectionLimit: 10,  
 };
 
-export var db;
+export var db = mysql.createPool(configuration);
 
-function handleDisconnect() {
-	db = mysql.createConnection(configuration);
+db.on("error", function (err) {
+  console.error("Database error:", err);
+  if (err.code === "PROTOCOL_CONNECTION_LOST") {
+    console.log("Connection lost. Attempting to reconnect...");
+    handleReconnect();
+  } else {
+    throw err;
+  }
+});
 
-	db.connect(function (err) {
-		if (err) {
-			console.log("error when connecting to db:", err);
-			setTimeout(handleDisconnect, 2000);
-		} else {
-			console.log("connection is successfull");
-		}
-	});
-	db.on("error", function (err) {
-		console.log("db error", err);
-		if (err.code === "PROTOCOL_CONNECTION_LOST") {
-			handleDisconnect();
-		} else {
-			throw err;
-		}
-	});
+function handleReconnect() {
+  db.getConnection(function (err, connection) {
+    if (err) {
+      console.error("Error getting a connection from the pool:", err);
+      setTimeout(handleReconnect, 2000);
+    } else if (connection) {
+      console.log("Successfully reconnected to the database!");
+      connection.release();
+    }
+  });
 }
-handleDisconnect();
+
+function keepAlive() {
+  db.query('SELECT 1', (err, results) => {
+    if (err) {
+      console.error('Error pinging database:', err);
+    } else {
+      console.log('Database connection is alive');
+    }
+  });
+}
+setInterval(keepAlive, 60000); 
